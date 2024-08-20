31d2301608eeb43d63e89b85eb99e10547be5515
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-08-20 16:28:40 +0530

    1st api ✅

--- server/src/controller/totalSalesOverTIme.controller.js
+++ server/src/controller/totalSalesOverTIme.controller.js
@@ -9,20 +9,20 @@ import mongoose from "mongoose";
 const dailySales = async (req, res) => {
   try {
     // Fetch all documents from the "shopifyOrders" collection
-    const db = mongoose.connection.useDb("RQ_Analytics"); 
+    const db = mongoose.connection.useDb("RQ_Analytics");
     const collection = db.collection("shopifyOrders");
     const pipeline = [
-        {
-            $addFields: {
-                createdAtDate: { $dateFromString: { dateString: "$created_at" } }
-            }
+      {
+        $addFields: {
+          createdAtDate: { $dateFromString: { dateString: "$created_at" } },
         },
+      },
       {
         $group: {
           _id: {
             year: { $year: "$createdAtDate" },
             month: { $month: "$createdAtDate" },
-            day: { $dayOfMonth: "$createdAtDate" }
+            day: { $dayOfMonth: "$createdAtDate" },
           },
           totalSales: { $sum: { $toDouble: "$total_price" } },
         },
@@ -50,48 +50,126 @@ const dailySales = async (req, res) => {
   }
 };
 
-const monthlySales  = async (req, res) => {
-    try {
-      // Fetch all documents from the "shopifyOrders" collection
-      const db = mongoose.connection.useDb("RQ_Analytics"); 
-      const collection = db.collection("shopifyOrders");
-      const pipeline = [
-          {
-              $addFields: {
-                  createdAtDate: { $dateFromString: { dateString: "$created_at" } }
-              }
-          },
-        {
-          $group: {
-            _id: {
-              year: { $year: "$createdAtDate" },
-              month: { $month: "$createdAtDate" },
-              day: { $dayOfMonth: "$createdAtDate" }
-            },
-            totalSales: { $sum: { $toDouble: "$total_price" } },
-          },
+const monthlySales = async (req, res) => {
+  try {
+    // Fetch all documents from the "shopifyOrders" collection
+    const db = mongoose.connection.useDb("RQ_Analytics");
+    const collection = db.collection("shopifyOrders");
+    const pipeline = [
+      {
+        $addFields: {
+          createdAtDate: { $dateFromString: { dateString: "$created_at" } },
         },
-        {
-          $sort: {
-            "_id.year": 1,
-            "_id.month": 1,
-            "_id.day": 1,
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAtDate" },
+            month: { $month: "$createdAtDate" },
           },
+          totalSales: { $sum: { $toDouble: "$total_price" } },
         },
-      ];
-  
-      // Fetch documents
-      const tt = await collection.aggregate(pipeline).toArray();
-  
-      if (!tt || tt.length === 0) {
-        res.status(404).json("couldn't get and calculate the data");
-      }
-  
-      res.status(200).json(tt);
-    } catch (err) {
-      console.error("Error fetching sales data:", err.message);
-      res.status(500).send("Server Error");
-    }
-  };
+      },
+      {
+        $sort: {
+          "_id.year": 1,
+          "_id.month": 1,
+        },
+      },
+    ];
 
-export { dailySales };
+    // Fetch documents
+    const tt = await collection.aggregate(pipeline).toArray();
+
+    if (!tt || tt.length === 0) {
+      res.status(404).json("couldn't get and calculate the data");
+    }
+
+    res.status(200).json(tt);
+  } catch (err) {
+    console.error("Error fetching sales data:", err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+const quarterlySales = async (req, res) => {
+  try {
+    // Fetch all documents from the "shopifyOrders" collection
+    const db = mongoose.connection.useDb("RQ_Analytics");
+    const collection = db.collection("shopifyOrders");
+    const pipeline = [
+      {
+        $addFields: {
+          createdAtDate: { $dateFromString: { dateString: "$created_at" } },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAtDate" },
+            quarter: { $ceil: { $divide: [{ $month: "$createdAtDate" }, 3] } },
+          },
+          totalSales: { $sum: { $toDouble: "$total_price" } },
+        },
+      },
+      {
+        $sort: {
+          "_id.year": 1,
+          "_id.quarter": 1,
+        },
+      },
+    ];
+
+    // Fetch documents
+    const tt = await collection.aggregate(pipeline).toArray();
+
+    if (!tt || tt.length === 0) {
+      res.status(404).json("couldn't get and calculate the data");
+    }
+
+    res.status(200).json(tt);
+  } catch (err) {
+    console.error("Error fetching sales data:", err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+const yearlySales = async (req, res) => {
+  try {
+    // Fetch all documents from the "shopifyOrders" collection
+    const db = mongoose.connection.useDb("RQ_Analytics");
+    const collection = db.collection("shopifyOrders");
+    const pipeline = [
+      {
+        $addFields: {
+          createdAtDate: { $dateFromString: { dateString: "$created_at" } },
+        },
+      },
+      {
+        $group: {
+          _id: { year: { $year: "$createdAtDate" } },
+          totalSales: { $sum: { $toDouble: "$total_price" } },
+        },
+      },
+      {
+        $sort: {
+          "_id.year": 1, // Sort by year in ascending order
+        },
+      },
+    ];
+
+    // Fetch documents
+    const tt = await collection.aggregate(pipeline).toArray();
+
+    if (!tt || tt.length === 0) {
+      res.status(404).json("couldn't get and calculate the data");
+    }
+
+    res.status(200).json(tt);
+  } catch (err) {
+    console.error("Error fetching sales data:", err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+export { dailySales, monthlySales, quarterlySales, yearlySales };
--- server/src/routes/totalSalesOverTime.routes.js
+++ server/src/routes/totalSalesOverTime.routes.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
-import { dailySales } from "../controller/totalSalesOverTIme.controller.js";
+import { dailySales, monthlySales, quarterlySales, yearlySales } from "../controller/totalSalesOverTIme.controller.js";
 
 
 const router = Router();
-
-router.route("/test").get(dailySales);
+// 
+router.route("/dailySales").get(dailySales);
+router.route("/monthlySales").get(monthlySales);
+router.route("/querterlySales").get(quarterlySales);
+router.route("/yearlySales").get(yearlySales);
 
 
 export default router
